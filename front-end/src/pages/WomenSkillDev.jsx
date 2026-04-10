98ed8f00495011ad2af1377aac5c235f7a3ed50d
Subikshaa Trust <subikshaatrust.org@gmail.com>
2026-04-11 00:50:57 +0530

    Final Fix: Remove logo shadows to prevent 'white ring' effect

--- front-end/src/pages/WomenSkillDev.jsx
+++ front-end/src/pages/WomenSkillDev.jsx
@@ -124,11 +124,7 @@ const WomenSkillDev = () => {
                                     height: '180px',
                                     width: '180px',
                                     borderRadius: '50%',
-                                    objectFit: 'contain',
-                                    backgroundColor: 'white',
-                                    border: '1px solid rgba(0,0,0,0.08)',
-                                    padding: '8px',
-                                    boxShadow: '0 8px 24px rgba(0,0,0,0.12)'
+                                    objectFit: 'contain'
                                 }}
                             />
                         </div>
