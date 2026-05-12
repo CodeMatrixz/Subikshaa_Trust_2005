b1b6df1fa95d2f47e4a42f9fb916660c22eec818
Subikshaa Trust <subikshaatrust.org@gmail.com>
2026-05-13 00:09:51 +0530

    Configure GitHub Pages deployment for frontend

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Subikshaa_Trust_2005/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
