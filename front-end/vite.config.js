f2cf86b64d9efad959a4b5eb03d55601d9a891c0
Subikshaa Trust <subikshaatrust.org@gmail.com>
2026-05-20 11:21:20 +0530

    Revert GitHub Pages base URL for Vercel deployment

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Subikshaa_Trust_2005/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
